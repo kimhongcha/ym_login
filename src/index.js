1b836bfbbe3001c9fde504d219862910250d0589
ckdballl <74363398+ckdballl@users.noreply.github.com>
2020-12-02 20:29:21 +0900

    Delete index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import IndexComponent from './IndexComponent';
-
-ReactDOM.render(<IndexComponent />, document.getElementById('root'));
